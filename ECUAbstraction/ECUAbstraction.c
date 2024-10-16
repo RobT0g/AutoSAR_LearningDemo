b67bb6f083b25c5196746d26008d12f7259982dc
Robson Luan <robsonluan273@hotmail.com>
2024-10-15 21:56:55 -0300

    One more class

--- ECUAbstraction/ECUAbstraction.c
+++ ECUAbstraction/ECUAbstraction.c
@@ -1,7 +1,7 @@
 #include "Rte_TempHBridgeSWC.h"
 
 void ReadTemperature_Function(){
-    //This is the main function of the Temperature Runnable. It is meant to trigger the ADC conversion and get a read of the current Temperature Value
+    /* This is the main function of the Temperature Runnable. It is meant to trigger the ADC conversion and get a read of the current Temperature Value */
     uint16 ADCResult;
     float PinVoltage, Temperature;
 
@@ -16,5 +16,30 @@ void ReadTemperature_Function(){
     /* This conversion to the actual temperature value is also dependent on the HW and is tipically done on the application, so this is only an example */
     Temperature = PinVoltage*100;
 
-    
+    /* After the temperature is calculated, it is set on the provider port via the RTE function */
+    /**
+     * Rte_Write_ is standard for all port write operations
+     * P_Temperature_ is the port in which the write is being done
+     * Temperature is the variable prototype being updated (specified on the interface which in this case is I_Temperature)
+     */
+    Rte_Write_P_Temperature_Temperature(Temperature);
+}
+
+void StartMotor_Function(uint8 FanSpeed){
+    /* This is the main function of the runnable StartMotor. It is responsible for making the motor run at certain speeds */
+    if(FanSpeed < 50){ 
+        /* Runs the motor to run at half speed */
+        Dio_WriteChannel(10, STD_HIGH);
+        Dio_WriteChannel(11, STD_LOW);
+    } else{
+        /* Run at full speed */
+        Dio_WriteChannel(10, STD_HIGH);
+        Dio_WriteChannel(11, STD_HIGH);
+    }
+}
+
+void StopMotor_Function(){
+    /* This is the main function of the runnable StopMotor. It is make the motor stop */
+    Dio_WriteChannel(10, STD_LOW);
+    Dio_WriteChannel(11, STD_LOW);
 }
